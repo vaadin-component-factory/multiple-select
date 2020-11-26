7fb6031d0d90708b5352ad7353290136140bd4ad
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-11-26 13:31:41 +0200

    Enable toggling of all-selected-items display (#3)
    By default, only the first selected value is displayed in the field, with the number of additionally selected values (N) is indicated as "(+N other(s))".
    
    This PR adds the possibility of changing this behavior by setting the displayAllSelected property to true, in which case all selected items will be displayed, comma-separated, with ellipsis if more items are present than fits the component.

--- src/test/java/com/vaadin/componentfactory/MultipleSelectView.java
+++ src/test/java/com/vaadin/componentfactory/MultipleSelectView.java
@@ -1,19 +1,70 @@
 package com.vaadin.componentfactory;
 
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 
 @Route("")
-public class MultipleSelectView extends Div {
+public class MultipleSelectView extends VerticalLayout {
 
     public MultipleSelectView() {
         MultipleSelect<String> multipleSelect = new MultipleSelect<>();
-        multipleSelect.setItems("Jose", "Manolo", "Pedro");
+        multipleSelect.setItems("Jose", "Manolo", "Pedro", "Luis");
 
         multipleSelect.setLabel("MultipleSelect");
         multipleSelect.setPlaceholder("Select multiple items");
-        multipleSelect.setWidth("25%");
+        multipleSelect.setWidth("200px");
 
         add(multipleSelect);
+
+        add(new Span("Toggle DisplayAllSelected:"), new Button(
+                "Toggle DisplayAllSelected",
+                e -> multipleSelect.setDisplayAllSelected(
+                !multipleSelect.isDisplayAllSelected())));
+
+        HorizontalLayout extraItemsAdjustLayout = getExtraItemsAdjustLayout(
+                multipleSelect);
+        add(extraItemsAdjustLayout);
+
+        HorizontalLayout widthAdjustLayout = getWidthAdjustLayout(
+                multipleSelect);
+        add(widthAdjustLayout);
+    }
+
+    private HorizontalLayout getExtraItemsAdjustLayout(
+            MultipleSelect<String> multipleSelect) {
+        TextField singularString = new TextField("Singular String");
+        singularString.setValue("other");
+        TextField pluralString = new TextField("Plural String");
+        pluralString.setValue("others");
+        Button setExtraItemsCountText = new Button("Set ExtraItemsCountText",
+                e -> multipleSelect.setExtraItemsCountText(
+                        singularString.getValue(), pluralString.getValue()));
+
+        HorizontalLayout extraItemsAdjustLayout = new HorizontalLayout(
+                singularString, pluralString, setExtraItemsCountText);
+        extraItemsAdjustLayout
+                .setDefaultVerticalComponentAlignment(Alignment.END);
+        return extraItemsAdjustLayout;
+    }
+
+    private HorizontalLayout getWidthAdjustLayout(
+            MultipleSelect<String> multipleSelect) {
+        IntegerField width = new IntegerField("Width (in pixels)");
+        width.setValueChangeMode(ValueChangeMode.ON_CHANGE);
+        width.setValue(200);
+        width.setMin(0);
+        Button setWidth = new Button("Set width",
+                e -> multipleSelect.setWidth(width.getValue() + "px"));
+
+        HorizontalLayout widthAdjustLayout = new HorizontalLayout(width,
+                setWidth);
+        widthAdjustLayout.setDefaultVerticalComponentAlignment(Alignment.END);
+        return widthAdjustLayout;
     }
 }
